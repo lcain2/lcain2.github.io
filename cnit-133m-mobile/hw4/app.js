ce5bd437b1c015509d64d1e0490e509b3a6945a3
lcain2 <142449845+lcain2@users.noreply.github.com>
2024-11-25 13:33:19 -0800

    service worker v2

--- cnit-133m-mobile/hw4/app.js
+++ cnit-133m-mobile/hw4/app.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
     window.addEventListener('load', () => {
-	   navigator.serviceWorker.register('./service-worker.js')
+	   navigator.serviceWorker.register('./service-worker-2.js')
 	      .then(registration => console.log('SW registered'))
 		  .catch(err => console.log(`SW not registered - Error: ${err}`))
     })
