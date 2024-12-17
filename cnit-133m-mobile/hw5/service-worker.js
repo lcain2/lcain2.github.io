4810930d637784fee85a3b28adfdb156cfbe3db3
lcain2 <142449845+lcain2@users.noreply.github.com>
2024-12-16 23:52:05 -0800

    Web hw5
    Web hw5

---
+++ cnit-133m-mobile/hw5/service-worker.js
@@ -0,0 +1,47 @@
+var CACHE_VERSION = 'sg-v1';
+var CACHE_FILES = [
+    'images/favicon.ico',
+    'images/ram-logo.png',
+    'images/bg-gold.jpg',
+    'images/bg-silver-600.jpg',
+    'images/bg-silver-1200.jpg', 
+    'css/hw4.css', 
+    'app.js',
+    'index.html'
+];
+
+self.addEventListener('install', event => {
+    console.log('SW installed');
+    event.waitUntil(
+        caches
+        .open(CACHE_VERSION)
+        .then(cache => {
+            console.log('SW caching files');
+            cache.addAll(CACHE_FILES)
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('SW activated');
+    event.waitUntil(
+        caches.keys().then(keyNames => {
+            return Promise.all(
+                keyNames.map(key => {
+                    if(key !== CACHE_VERSION) {
+                        console.log('SW clearing old caches');
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('SW fetching');
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
